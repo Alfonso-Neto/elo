18e70c9b17484de605391aee808ea6fc2f707992
Alfonso-Neto <netoalfonso@outlook.com>
2026-08-07 22:52:30 -0300

    test(ui): stabilize lazy route assertions

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,7 @@
 import '@testing-library/jest-dom/vitest'
+import { configure } from '@testing-library/react'
+
+configure({ asyncUtilTimeout: 10_000 })
 
 Object.defineProperty(window, 'scrollTo', { value: () => undefined, writable: true })
 Object.defineProperty(window, 'matchMedia', {
